5b289838abc073379f9f0f33fec12c42aac7adc5
AbbasiJaved <ajaved@graduate.utm.my>
2023-06-01 23:12:24 +0500

    removed port number from api url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 //export const API_URL = "http://localhost:3000/api";
-export const API_URL = "https://cvms.onrender.com:10000/api";
+//export const API_URL = "http://localhost:8080/api";
+export const API_URL = "https://cvms.onrender.com/api";
